b91730919eadeda5bc0020dfb8b7fde991b5abca
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-29 21:31:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+API_ID = "29245477" #API ID get it from telegram.org
+API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
+BOT_TOKEN = "7831174760:AAFKWWGJA2McsmSlrZUnYvXfy_BIvMsPf70" #Bot Token get it from @BotFather
 URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "https://litter.catbox.moe/m4c4wu.jpg" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+MONGO_URI = "mongodb+srv://BladexRobot:az50i6VCbZnez34y@cluster0.8uydmmp.mongodb.net/" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "7654385403" #Admin ID

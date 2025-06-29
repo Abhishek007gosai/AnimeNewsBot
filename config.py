35979325ff9b54bca01de47cf1a5b41370e8d936
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-29 19:17:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
+API_ID = "29245477" #API ID get it from telegram.org
+API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
 BOT_TOKEN = "" #Bot Token get it from @BotFather
 URL_A = "" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
-START_PIC = "" #Startup Pic
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = "" #Admin ID
+START_PIC = "https://litter.catbox.moe/m4c4wu.jpg" #Startup Pic
+MONGO_URI = "mongodb+srv://gutsxrobot:4LfFzZZ4T3VpJzzW@cluster0.r4wywvl.mongodb.net/" #Databse uri get it from https://www.mongodb.com/
+ADMINS = "7654385403" #Admin ID
